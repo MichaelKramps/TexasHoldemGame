0eb1196a749b627d3c2ca62b04293e798d8a314e
Michael Kramps <mdk989@gmail.com>
2015-01-15 14:32:51 -0600

    Update play.py
    betting prompt won't error for non int values.

--- play.py
+++ play.py
@@ -16,65 +16,22 @@ class texasHoldem:
         self.table.newHand()
         p1 = self.table.players["Player1"]
         while (p1.chips > -1 and p1.chips < 2001):
-            for key in self.table.players: ##Unfold all players
-                self.table.players[key].folded = False
-            if p1.holeCards == []:
-                self.table.newHand()
-            self.winningHand = -1 ##Reset Winning Hand, these resets should probably be moved to the table class when I rewrite this
+            self.winningHand = -1 ##Reset Winning Hand
             self.winners = [] #Reset winners
-            preFlopWager = input("How many chips would you like to bet this round? (You have " + str(p1.chips) + " chips) ")
-            self.wager(p1, int(preFlopWager))
-            if int(preFlopWager) > 0:
-                for key in self.table.players:
-                    if random.random() < 0.9 and key != "Player1":
-                        self.wager(self.table.players[key], int(preFlopWager))
-                    elif key == "Player1":
-                        phantom = 0 ##got to be a better way to do this
-                    else:
-                        self.table.players[key].folded = True
-                        print (key + " folds")
+            self.table.newHand() ##Deal new hole cards
+            self.preFlopBet()
             self.table.dealFlop()
             self.table.displayPot()
             self.table.displayMyHand()
-            flopWager = input("How many chips would you like to bet this round? (You have " + str(p1.chips) + " chips) ")
-            self.wager(p1, int(flopWager))
-            if int(flopWager) > 0:
-                for key in self.table.players:
-                    if random.random() < 0.98 and key != "Player1":
-                        self.wager(self.table.players[key], int(flopWager))
-                    elif key == "Player1":
-                        phantom = 0
-                    else:
-                        self.table.players[key].folded = True
-                        print (key + " folds")
+            self.flopBet()
             self.table.dealTurn()
             self.table.displayPot()
             self.table.displayMyHand()
-            turnWager = input("How many chips would you like to bet this round? (You have " + str(p1.chips) + " chips) ")
-            self.wager(p1, int(turnWager))
-            if int(turnWager) > 0:
-                for key in self.table.players:
-                    if random.random() < 0.9 and key != "Player1":
-                        self.wager(self.table.players[key], int(turnWager))
-                    elif key == "Player1":
-                        phantom = 0
-                    else:
-                        self.table.players[key].folded = True
-                        print (key + " folds")
+            self.turnBet()
             self.table.dealRiver()
             self.table.displayPot()
             self.table.displayMyHand()
-            riverWager = input("How many chips would you like to bet this round? (You have " + str(p1.chips) + " chips) ")
-            self.wager(p1, int(riverWager))
-            if int(riverWager) > 0:
-                for key in self.table.players:
-                    if random.random() < 0.9 and key != "Player1":
-                        self.wager(self.table.players[key], int(riverWager))
-                    elif key == "Player1":
-                        phantom = 0
-                    else:
-                        self.table.players[key].folded = True
-                        print (key + " folds")
+            self.riverBet()
             for key in self.table.players:
                 if self.table.players[key].folded == False:
                     self.table.players[key].finalHand = self.evaluator.evaluate(self.table.players[key].holeCards, self.table.communityCards)
@@ -112,7 +69,86 @@ class texasHoldem:
     def wager(self, player, wager):
         player.bet(wager)
         self.table.addToPot(wager)
-
+    def preFlopBet(self):
+        p1 = self.table.players["Player1"]
+        preFlopWager = input("How many chips would you like to bet this round? (You have " + str(p1.chips) + " chips) ")
+        try:
+            preFlopWager = int(preFlopWager)
+        except:
+            try:
+                preFlopWager = int(float(preFlopWager))
+            except:
+                preFlopWager = 0
+        self.wager(p1, preFlopWager)
+        if preFlopWager > 0:
+            for key in self.table.players:
+                if random.random() < 0.9 and key != "Player1":
+                    self.wager(self.table.players[key], preFlopWager)
+                elif key == "Player1":
+                    phantom = 0 ##got to be a better way to do this
+                else:
+                    self.table.players[key].folded = True
+                    print (key + " folds")
+    def flopBet(self):
+        p1 = self.table.players["Player1"]
+        flopWager = input("How many chips would you like to bet this round? (You have " + str(p1.chips) + " chips) ")
+        try:
+            flopWager = int(flopWager)
+        except:
+            try:
+                flopWager = int(float(flopWager))
+            except:
+                flopWager = 0
+        self.wager(p1, flopWager)
+        if flopWager > 0:
+            for key in self.table.players:
+                if random.random() < 0.98 and key != "Player1":
+                    self.wager(self.table.players[key], flopWager)
+                elif key == "Player1":
+                    phantom = 0
+                else:
+                    self.table.players[key].folded = True
+                    print (key + " folds")
+    def turnBet(self):
+        p1 = self.table.players["Player1"]
+        turnWager = input("How many chips would you like to bet this round? (You have " + str(p1.chips) + " chips) ")
+        try:
+            turnWager = int(turnWager)
+        except:
+            try:
+                turnWager = int(float(turnWager))
+            except:
+                turnWager = 0
+        self.wager(p1, turnWager)
+        if turnWager > 0:
+            for key in self.table.players:
+                if random.random() < 0.9 and key != "Player1":
+                    self.wager(self.table.players[key], turnWager)
+                elif key == "Player1":
+                    phantom = 0
+                else:
+                    self.table.players[key].folded = True
+                    print (key + " folds")
+    def riverBet(self):
+        p1 = self.table.players["Player1"]
+        riverWager = input("How many chips would you like to bet this round? (You have " + str(p1.chips) + " chips) ")
+        try:
+            riverWager = int(riverWager)
+        except:
+            try:
+                riverWager = int(float(riverWager))
+            except:
+                riverWager = 0
+            self.wager(p1, riverWager)
+            if riverWager > 0:
+                for key in self.table.players:
+                    if random.random() < 0.9 and key != "Player1":
+                        self.wager(self.table.players[key], riverWager)
+                    elif key == "Player1":
+                        phantom = 0
+                    else:
+                        self.table.players[key].folded = True
+                        print (key + " folds")
     def findWinner(self, wins):
         self.winningHand = 0
         firstWinner = wins[0]
